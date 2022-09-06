86d6b22a4a05816613a48c91dc3bda0c010b12bb
StephenTurk90 <stephenjturk@gmail.com>
2022-09-06 19:46:30 -0400

    added templates/habit base.html and home.html, updated config/urls.py and habit_tracker/urls.py, views.py, and base.html

--- habit_tracker/urls.py
+++ habit_tracker/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.contrib import admin
+from django.conf import settings
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.Habit, name = 'Habit'),
+    path("", views.home, name="home"),
+    # You need this for django-registration-redux
+    path("auth/", include("registration.backends.simple.urls")),
+    path("admin/", admin.site.urls),
+    path("habits/", views.habit, name="habit"),
 ]
--- habit_tracker/views.py
+++ habit_tracker/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Habit
 from .forms import HabitForm
 from django.shortcuts import redirect
+
+
+def home(request):
+    if request.user.is_authenticated:
+        return redirect("habit")
+    return render(request, "habit/home.html")
+
+
+@login_required
+def habit(request):
+    sort_by = request.GET.get("sort") or "title"
+    habit = Habit.objects.annotate(
+        favorited=Exists(
+            CustomUser.objects.filter(
+                favorite_albums=OuterRef("pk"), pk=request.user.pk
+            )
+        )
+    ).order_by(sort_by)
+    return render(
+        request, "music/list_albums.html", {"albums": albums, "sort_by": sort_by}
+    )
+
+
 
 class HabitForm:
     class Meta:
