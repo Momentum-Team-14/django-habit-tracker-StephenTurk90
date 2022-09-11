e9abd22ae0781b893a9ff0cc5bf310dcaef4aba6
StephenTurk90 <stephenjturk@gmail.com>
2022-09-11 16:25:11 -0400

    updated views.py

--- habit_tracker/views.py
+++ habit_tracker/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Habit
-from .forms import HabitForm
+from .models import User, Habit, DailyRecord
+from .forms import HabitForm, DailyRecordForm
 from django.shortcuts import redirect
 
 
